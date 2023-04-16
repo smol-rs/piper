ac7f149152908a2083e2bd10c1c4447a2618b261
jtnunley <jtnunley01@gmail.com>
2023-04-16 08:19:28 -0700

    Add more tests

--- tests/pipe.rs
+++ tests/pipe.rs
@@ -2,6 +2,7 @@ use easy_parallel::Parallel;
 use futures_lite::{future, prelude::*};
 use piper::pipe;
 
+use std::task::{Context, Poll};
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -57,4 +58,102 @@ fn zero_cap_pipe() {
 #[test]
 fn large_pipe() {
     let _ = pipe(core::usize::MAX);
+}
+
+#[test]
+fn dropping_reader_wakes_writer() {
+    let (r, mut w) = pipe(1);
+
+    Parallel::new()
+        .add(move || {
+            sleep(Duration::from_millis(100));
+            drop(r);
+        })
+        .add(move || {
+            future::block_on(w.write_all(&[0u8])).unwrap();
+            sleep(Duration::from_millis(200));
+            with_cx(|cx| {
+                assert_eq!(w.poll_fill_bytes(cx, &[0u8]), Poll::Ready(0));
+                assert!(w.is_closed());
+            });
+        })
+        .run();
+}
+
+#[test]
+fn dropping_writer_wakes_reader() {
+    let (mut r, w) = pipe(1);
+
+    Parallel::new()
+        .add(move || {
+            drop(w);
+        })
+        .add(move || {
+            sleep(Duration::from_millis(100));
+            with_cx(|cx| {
+                assert_eq!(r.poll_drain_bytes(cx, &mut [0u8]), Poll::Ready(0));
+                assert!(r.is_closed());
+            });
+        })
+        .run();
+}
+
+#[test]
+fn len() {
+    let (mut r, mut w) = pipe(100);
+
+    assert_eq!(r.len(), 0);
+    assert_eq!(w.len(), 0);
+    assert!(r.is_empty());
+    assert!(w.is_empty());
+    assert!(!r.is_full());
+    assert!(!w.is_full());
+
+    let buf = [0u8; 10];
+    future::block_on(w.write_all(&buf)).unwrap();
+
+    assert_eq!(r.len(), 10);
+    assert_eq!(w.len(), 10);
+    assert!(!r.is_empty());
+    assert!(!w.is_empty());
+    assert!(!r.is_full());
+    assert!(!w.is_full());
+
+    // Fill up the pipe
+    let buf = [0u8; 90];
+    future::block_on(w.write_all(&buf)).unwrap();
+
+    assert_eq!(r.len(), 100);
+    assert_eq!(w.len(), 100);
+    assert!(!r.is_empty());
+    assert!(!w.is_empty());
+    assert!(r.is_full());
+    assert!(w.is_full());
+
+    // Read some bytes.
+    let mut buf = [0u8; 15];
+    future::block_on(r.read_exact(&mut buf)).unwrap();
+
+    assert_eq!(r.len(), 85);
+    assert_eq!(w.len(), 85);
+    assert!(!r.is_empty());
+    assert!(!w.is_empty());
+    assert!(!r.is_full());
+    assert!(!w.is_full());
+
+    // Write some more to loop around the capacity.
+    let buf = [0u8; 10];
+    future::block_on(w.write_all(&buf)).unwrap();
+
+    assert_eq!(r.len(), 95);
+    assert_eq!(w.len(), 95);
+    assert!(!r.is_empty());
+    assert!(!w.is_empty());
+    assert!(!r.is_full());
+    assert!(!w.is_full());
+}
+
+fn with_cx<R, F: FnOnce(&mut Context<'_>) -> R>(f: F) -> R {
+    let mut f = Some(f);
+    future::block_on(future::poll_fn(|cx| Poll::Ready((f.take().unwrap())(cx))))
 }
